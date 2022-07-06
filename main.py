a3219ef993b8306358b6ad4a96825f6cd827d147
GarborSerey <garborfersru@gmail.com>
2022-07-06 19:27:04 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from icrawler.builtin import GoogleImageCrawler
+
+
+def google_img_downloader():
+    filters = dict(
+        type='photo',
+        color='blackandwhite',
+        size='large',
+        # license='noncommercial, commercial',
+        date=((2020, 1, 1), (2022, 5, 14))
+    )
+    crawler = GoogleImageCrawler(storage={'root_dir': './img'})
+    # crawler.crawl(keyword='mr.robot', max_num=5)
+    crawler.crawl(
+        keyword='Omsk',
+        max_num=5,
+        # min_size=(1000, 1000),
+        overwrite=True,
+        # filters=filters,
+        file_idx_offset='auto',
+    )
+
+
+def main():
+    google_img_downloader()
+
+
+if __name__ == '__main__':
+    main()
